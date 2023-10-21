8830098d234f0696b9bf730ef1dc2d1ca576a681
amanrajtharani <araj32279@gmail.com>
2023-10-21 08:04:32 -0700

    rotate.py

---
+++ rotate_array.py
@@ -0,0 +1,15 @@
+#Rotate array
+arr = []
+num = 0
+for i in range(5):
+    item = int(input("Enter element : "))
+    arr.append(item)
+rt_num = int(input("Enter the number from which you want to rotate : "))
+for i in range(rt_num,len(arr)):
+    for k in range(rt_num,len(arr)-1):
+        if arr[i]<arr[k]:
+            num = arr[i]
+            arr[i] = arr[k]
+            arr[k] = num
+
+
