03771fe45d88d6783177170774a7fc29cbc0f628
amanrajtharani <araj32279@gmail.com>
2023-11-16 07:59:59 -0800

    res.py

--- res.py
+++ res.py
@@ -1,9 +1,10 @@
 class user:
-    def __init__(self, f_name,l_name,age,caste):
+    def __init__(self, f_name,l_name,age,caste,log_attempts):
         self.f_name = f_name
         self.l_name = l_name
         self.age = age
         self.caste = caste
+        self.log_attempts = log_attempts
     def desc(self):
         print("first name ",self.f_name)
         print("last name ",self.l_name)
@@ -11,5 +12,14 @@ class user:
         print("caste ",self.caste)
     def greet(self):
         print("hello ",self.f_name," ",self.l_name)
-a = user("aman","raj",21,"tharani")
-a.desc()
+    def increment_log_attempts(self):
+        self.log_attempts += 1
+    def reset_log_attempts(self):
+        self.log_attempts = 0
+a = user("aman","raj",21,"Tharani",5)
+a.increment_log_attempts()
+a.increment_log_attempts()
+a.increment_log_attempts()
+print(a.log_attempts)
+a.reset_log_attempts()
+print(a.log_attempts)
