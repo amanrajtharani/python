152d6e6f4302635e8304184d17731b0d03e3b6d6
amanrajtharani <araj32279@gmail.com>
2023-11-17 07:34:06 -0800

    res.py

--- res.py
+++ res.py
@@ -1,25 +1,36 @@
-class user:
-    def __init__(self, f_name,l_name,age,caste,log_attempts):
+class User:
+    def __init__(self, f_name, l_name, age, caste, log_attempts):
         self.f_name = f_name
         self.l_name = l_name
         self.age = age
         self.caste = caste
         self.log_attempts = log_attempts
+
     def desc(self):
-        print("first name ",self.f_name)
-        print("last name ",self.l_name)
-        print("age ",self.age)
-        print("caste ",self.caste)
+        print("first name ", self.f_name)
+        print("last name ", self.l_name)
+        print("age ", self.age)
+        print("caste ", self.caste)
+
     def greet(self):
-        print("hello ",self.f_name," ",self.l_name)
+        print("hello ", self.f_name, " ", self.l_name)
+
     def increment_log_attempts(self):
         self.log_attempts += 1
+
     def reset_log_attempts(self):
         self.log_attempts = 0
-a = user("aman","raj",21,"Tharani",5)
-a.increment_log_attempts()
-a.increment_log_attempts()
-a.increment_log_attempts()
-print(a.log_attempts)
-a.reset_log_attempts()
-print(a.log_attempts)
+
+    class Admin:
+        def __init__(self):
+            self.pr = []
+            self.pr.append("can add book")
+            self.pr.append("can delete book")
+            self.pr.append("can ban book")
+
+        def show_pr(self):
+            for i in range(0, len(self.pr)):
+                print(self.pr[i])
+
+a = User.Admin()
+a.show_pr()
